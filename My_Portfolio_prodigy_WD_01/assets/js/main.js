0eea8458cf371ce1099d4567912d3771c62bf878
Ajinkya Prakash Chirde <ajinkyachirde07@gmail.com>
2023-12-04 23:44:01 +0530

    Add files via upload

---
+++ My_Portfolio_prodigy_WD_01/assets/js/main.js
@@ -0,0 +1,36 @@
+/*SEEN*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/* REMOVE MENU MOBILE*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle,.my_resume, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text,.resume__img, .skills__img',{delay: 200}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 100}); 
